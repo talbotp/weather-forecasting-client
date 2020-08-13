c5ea993d6cce6efb13a1e243b6e9fb9c5a6aac9f
talbotp <parkertalbot2@gmail.com>
2020-08-13 18:40:21 +0100

    Added a functioning city to latitude/longitude function

--- src/weather_forecaster.erl
+++ src/weather_forecaster.erl
@@ -11,6 +11,7 @@
 %% API
 -export([
   init/0,
+  current_forecast/1,
   current_forecast/2
 ]).
 
@@ -24,6 +25,26 @@
 
 init() ->
   ibrowse:start().
+
+current_forecast(City) when is_binary(City) ->
+  {ok, ConversionURL} = get_env(city_conversion_url),
+  {ok, "200", _HttpParams, Json} = ibrowse:send_req(ConversionURL, [], ?GET),
+  JsonMap = jsx:decode(list_to_binary(Json)),
+  case get_duplicate_name_cities(City, JsonMap) of
+    [] ->
+      io:format("No results found for {City name}.");
+    CityList when length(CityList) >= 1 ->
+      lists:foreach(
+        fun(CityDataMap) ->
+          Latitude = maps:get(<<"lat">>, CityDataMap),
+          Longitude = maps:get(<<"lng">>, CityDataMap),
+          current_forecast(Latitude, Longitude)
+        end, CityList)
+  end;
+current_forecast(City) when is_list(City) ->
+  current_forecast(list_to_binary(City));
+current_forecast(City) when is_atom(City) ->
+  current_forecast(atom_to_list(City)).
 
 %%--------------------------------------------------------------------
 %% Send a get request to darksky, and return the json result, or error.
@@ -44,12 +65,13 @@ current_forecast(Latitude, Longitude) when ?is_number(Latitude) andalso ?is_numb
   HourlyPrecipProb    = maps:get(<<"precipProbability">>, HourlyDataMap),
 
   %% Return the binary summary of current forecast.
-  <<
+  Result = <<
     <<"Current weather - ">>/binary, CurrentSummary/binary,
     <<", Today we will see - ">>/binary, HourlySummary/binary,
     <<" with a ">>/binary, (number_to_binary(HourlyPrecipProb))/binary,
     <<"% chance of rain.">>/binary
-  >>.
+  >>,
+  io:format(binary_to_list(Result)).
 
 %%%===================================================================
 %%% Internal functions
@@ -73,6 +95,21 @@ query_darksky(Latitude, Longitude) ->
     _ ->
       error
   end.
+
+%%--------------------------------------------------------------------
+%% Given a city and a List of maps containing city information,
+%% return a list of cities with matching names.
+%%--------------------------------------------------------------------
+get_duplicate_name_cities(City, CityDataList) ->
+  lists:filter(
+    fun(CityDataMap) ->
+      case maps:find(<<"name">>, CityDataMap) of
+        {ok, CurrentCityName} ->
+          CurrentCityName =:= City;
+        error ->
+          false
+      end
+    end, CityDataList).
 
 get_env(Key) when is_atom(Key) ->
   application:get_env(weather_forecaster, Key).
